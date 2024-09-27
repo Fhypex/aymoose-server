917295f454b95f8feb9b8bce238c768ceb913f10
unknown <aydogdutaha01@gmail.com>
2024-09-27 22:07:56 +0300

    ilk commit

---
+++ src/main/java/gtu/cse/se/altefdirt/aymoose/AymooseApplication.java
@@ -0,0 +1,13 @@
+package gtu.cse.se.altefdirt.aymoose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AymooseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AymooseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gtu/cse/se/altefdirt/aymoose/AymooseApplicationTests.java
@@ -0,0 +1,13 @@
+package gtu.cse.se.altefdirt.aymoose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AymooseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
