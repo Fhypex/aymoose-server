c59bbba4c48adf271db9c2b232047eb04d172e89
mehmet.ozkul <mehmet.ozkul@tubitak.gov.tr>
2024-09-28 00:16:49 +0300

    Hello Controller added

---
+++ src/main/java/gtu/cse/se/altefdirt/aymoose/controller/HelloController.java
@@ -0,0 +1,14 @@
+
+package gtu.cse.se.altefdirt.aymoose.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("")
+    public String hello() {
+        return "Hello!";
+    }
+}
