f0c166c2c30b6548e22fb4c84136cc6a14569eb4
mehmet.ozkul <mehmet.ozkul@tubitak.gov.tr>
2024-09-29 14:13:41 +0300

    update hello controller 04

--- src/main/java/gtu/cse/se/altefdirt/aymoose/controller/HelloController.java
+++ src/main/java/gtu/cse/se/altefdirt/aymoose/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     
     @GetMapping("")
     public String hello() {
-        return "Hello, new user has been created! Daha da güzellesti, The better the newer. V3";
+        return "Hello, new user has been created! Daha da güzellesti, The better the newer. V4";
     }
 }
